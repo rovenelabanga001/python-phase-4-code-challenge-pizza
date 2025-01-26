ab9d39e572d41329050a4faf475fca49e8d01660
rovenelabanga <rovenelabanga2001@gmail.com>
2025-01-26 21:55:48 +0300

    adds get and delete requests to restaurants_by_id

--- server/app.py
+++ server/app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 from models import db, Restaurant, RestaurantPizza, Pizza
 from flask_migrate import Migrate
-from flask import Flask, request, make_response
+from flask import Flask, request, make_response, jsonify
 from flask_restful import Api, Resource
 import os
 
@@ -23,6 +23,55 @@ api = Api(app)
 @app.route("/")
 def index():
     return "<h1>Code challenge</h1>"
+
+@app.route("/restaurants", methods=['GET'])
+def restaurants():
+    restaurants = []
+
+    for restaurant in Restaurant.query.all():
+        restaurant_dict = restaurant.to_dict()
+        restaurants.append(restaurant_dict)
+    
+    response = make_response(
+        restaurants,
+        200
+    )
+
+    return response
+
+@app.route("/restaurants/<int:id>", methods=['GET', 'DELETE'])
+def restaurant_by_id(id):
+    restaurant = Restaurant.query.filter(Restaurant.id == id).first()
+
+    if restaurant is None:
+        response_body = {
+            "error" : "Restaurant not found"
+        }
+
+        response = make_response(
+            response_body,
+            404
+        )
+        return response
+
+    else:
+        if request.method == 'GET':
+            restaurant_dict = restaurant.to_dict()
+
+            response = make_response(restaurant_dict, 200)
+
+            return response
+            
+        elif request.method == 'DELETE':
+            db.session.delete(restaurant)
+            db.session.commit()
+
+            response = make_response(
+                "",
+                200
+            )
+
+            return response
 
 
 if __name__ == "__main__":
--- server/models.py
+++ server/models.py
@@ -21,8 +21,13 @@ class Restaurant(db.Model, SerializerMixin):
     address = db.Column(db.String)
 
     # add relationship
-
+    restaurant_pizzas = db.relationship('RestaurantPizza', back_populates = "restaurant", cascade = "all, delete-orphan")
+    pizzas = association_proxy("restaurant_pizzas", "pizza")
     # add serialization rules
+    serialize_rules = ("-restaurant_pizzas",)
+    # add serialization rules
+
+    
 
     def __repr__(self):
         return f"<Restaurant {self.name}>"
@@ -36,8 +41,12 @@ class Pizza(db.Model, SerializerMixin):
     ingredients = db.Column(db.String)
 
     # add relationship
+    restaurant_pizzas = db.relationship('RestaurantPizza', back_populates = "pizza", cascade="all, delete-orphan")
 
+    restaurants = association_proxy("restaurant_pizzas", "restaurant")
     # add serialization rules
+
+    serialize_rules = ('-restaurant_pizzas.pizza',)
 
     def __repr__(self):
         return f"<Pizza {self.name}, {self.ingredients}>"
@@ -49,11 +58,20 @@ class RestaurantPizza(db.Model, SerializerMixin):
     id = db.Column(db.Integer, primary_key=True)
     price = db.Column(db.Integer, nullable=False)
 
+    pizza_id = db.Column(db.Integer, db.ForeignKey('pizzas.id'))
+    restaurant_id = db.Column(db.Integer, db.ForeignKey('restaurants.id'))
+
     # add relationships
-
+    pizza = db.relationship('Pizza', back_populates = 'restaurant_pizzas')
+    restaurant = db.relationship('Restaurant', back_populates = 'restaurant_pizzas')
     # add serialization rules
-
+    serialize_rules = ("-pizza.restaurant_pizzas", "-restaurant.restaurant_pizzas")
     # add validation
+    @validates('price')
+    def validates_price(self, key, value):
+        if value < 1 or value > 30:
+            raise ValueError("Price must be between 1 and 30")
+        return value
 
     def __repr__(self):
         return f"<RestaurantPizza ${self.price}>"
